31217303b1c844bbe32eaebd4c825dea881fcbb3
blbrdv <95272917+blbrdv@users.noreply.github.com>
2023-12-18 17:04:12 +0700

    move from environment variables to args

---
+++ Args.cs
@@ -0,0 +1,24 @@
+﻿using Utility.CommandLine;
+
+namespace TGTopReactions;
+
+public static class Args
+{
+    [Argument('i', "id", "Application ID.")]
+    public static string ApiId { get; set; }
+    
+    [Argument('h', "hash", "Application Hash.")]
+    public static string ApiHash { get; set; }
+    
+    [Argument('p', "phone", "Account phone number.")]
+    public static string PhoneNumber { get; set; }
+    
+    [Argument('c', "channel", "Channel name.")]
+    public static string ChannelName { get; set; }
+    
+    [Argument('r', "reaction", "Reaction count limit.")]
+    public static int ReactionCountLimit { get; set; } = 5;
+    
+    [Argument('m', "minutes", "Time limit in minutes.")]
+    public static int MinutesTimeLimit { get; set; } = 525600;
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
 ﻿using TGTopReactions;
 using TL;
-using Config = tgreacts.Config;
+using Utility.CommandLine;
 
-using var client = new WTelegram.Client(Config.Auth);
+Arguments.Populate(typeof(Args), string.Join(" ", args));
+
+using var client = new WTelegram.Client(
+    param =>
+    {
+        switch (param)
+        {
+            case "api_id": return Args.ApiId;
+            case "api_hash": return Args.ApiHash;
+            case "phone_number": return Args.PhoneNumber;
+            case "verification_code": Console.Write("Code: "); return Console.ReadLine();
+            default: return null;
+        }
+    });
 var _ = await client.LoginUserIfNeeded();
 
-var channel = await client.Channels_GetChannelByUsername(Config.ChannelName);
+var channel = await client.Channels_GetChannelByUsername(Args.ChannelName);
 var history = await client.Messages_GetHistory(channel, 0, default, 0, 0);
 
 
 Dictionary<string, int> messages = new();
 var step = 0;
 
-while (history.Messages[0].Date >= DateTime.Today.AddMinutes(-Config.MinutesLimit))
+while (history.Messages[0].Date >= DateTime.Today.AddMinutes(-Args.MinutesTimeLimit))
 {
     await Task.Delay(1000);
 
@@ -28,10 +41,10 @@ while (history.Messages[0].Date >= DateTime.Today.AddMinutes(-Config.MinutesLimi
 
         var count = message.reactions.results.Sum(x => x.count);
 
-        if (count <= Config.ReactionCountLimit)
+        if (count <= Args.ReactionCountLimit)
             continue;
         
-        messages.Add($"https://t.me/{Config.ChannelName}/{messageBase.ID}", count);
+        messages.Add($"https://t.me/{Args.ChannelName}/{messageBase.ID}", count);
     }
 
     step += 20;
